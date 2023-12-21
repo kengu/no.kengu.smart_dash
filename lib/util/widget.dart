2907479afd78f11654dc4099954037c5274360c1
Kenneth Gulbrandsøy <kenneth.gulbrandsoy@entur.org>
2023-12-21 22:58:53 +0100

    Refactored homepage mobile layout

--- lib/util/widget.dart
+++ lib/util/widget.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_dash/scaffold/presentation/app/smart_dash_app_theme_data.dart';
 
-Size calculateTextSize(
-  BuildContext context, {
-  required String text,
-  required TextStyle style,
-}) {
-  final TextPainter textPainter = TextPainter(
-    text: TextSpan(text: text, style: style),
-    textDirection: Directionality.of(context),
-    textScaler: MediaQuery.of(context).textScaler,
-  )..layout(minWidth: 0, maxWidth: double.infinity);
-
-  return textPainter.size;
-}
-
 TextStyle getLegendTextStyle(BuildContext context) {
   final surfaceColor = Theme.of(context).navigationRailTheme.backgroundColor!;
   final legendTextColor = surfaceColor.lighten(0.2);
