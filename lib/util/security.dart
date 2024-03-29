9e7d2f7bca1d485728743b364806f9fc001cd266
kengu <kenneth@discoos.org>
2024-03-29 17:16:14 +0100

    Changed MacOsOptions to first_unlock in getHiveCipher to fix UnsupportedError when app in background

--- lib/util/security.dart
+++ lib/util/security.dart
@@ -3,23 +3,34 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive/hive.dart';
+import 'package:universal_io/io.dart';
 
 Future<HiveCipher> getHiveCipher(String key) async {
-  const secureStorage = FlutterSecureStorage();
+  final storage = _get();
   // if key not exists return null
-  final encryptionKeyString = await secureStorage.read(key: key);
+  final encryptionKeyString = await storage.read(key: key);
   if (encryptionKeyString == null) {
     final data = Hive.generateSecureKey();
-    await secureStorage.write(
+    await storage.write(
       key: key,
       value: base64UrlEncode(data),
     );
     debugPrint('Created Cipher for Hive key $key');
   }
-  final data = await secureStorage.read(key: key);
+  final data = await storage.read(key: key);
   if (data == null) {
     throw UnsupportedError('Unable to read from secure storage');
   }
   final encryptionKeyUint8List = base64Url.decode(data);
   return HiveAesCipher(encryptionKeyUint8List);
+}
+
+FlutterSecureStorage _get() {
+  if (Platform.isMacOS) {
+    return const FlutterSecureStorage(
+        mOptions: MacOsOptions(
+      accessibility: KeychainAccessibility.first_unlock,
+    ));
+  }
+  return const FlutterSecureStorage();
 }
